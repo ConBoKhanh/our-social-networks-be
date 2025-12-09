00f743ed64721065e2722a8a10beaedb761b15ff
Nguyễn Khánh Duy <112497178+ConBoKhanh@users.noreply.github.com>
2025-12-09 14:30:10 +0700

    duynk

---
+++ src/main/java/oursocialnetworks/oursocialnetworks/OurSocialNetworksApplication.java
@@ -0,0 +1,13 @@
+package oursocialnetworks.oursocialnetworks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OurSocialNetworksApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OurSocialNetworksApplication.class, args);
+    }
+
+}
---
+++ src/test/java/oursocialnetworks/oursocialnetworks/OurSocialNetworksApplicationTests.java
@@ -0,0 +1,13 @@
+package oursocialnetworks.oursocialnetworks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OurSocialNetworksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
